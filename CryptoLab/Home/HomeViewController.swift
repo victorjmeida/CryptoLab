c261bfd09aca91ff6da9a2ace8573f8b20218696
João Victor Almeida <joaovictorsilva.meida@gmail.com>
2025-09-09 23:10:07 -0300

    - Retactor Home for study.

--- CryptoLab/Home/HomeViewController.swift
+++ CryptoLab/Home/HomeViewController.swift
@@ -9,24 +9,5 @@ import UIKit
 
 class HomeViewController: UIViewController{
     
-    private let homeView = HomeView()
-    private let viewModel = HomeViewModel()
-    
-    override func loadView() {
-        self.view = homeView
-        homeView.backgroundColor = .backgroundColor
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        title = "Home"
 
-        // teste rápido: só pra ver no console
-        viewModel.load {
-        print("Total de moedas: \(self.viewModel.coins.count)")
-        if let error = self.viewModel.errorMessage {
-            print("Erro: \(error)")
-        }
-        }
-    }
 }
